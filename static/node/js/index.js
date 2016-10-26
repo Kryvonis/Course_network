79c5712b4b626a277095a10fb871dfda76a92dae
kryvonis <Kryvonis.artem@gmail.com>
2016-10-26 11:17:50 +0300

    refactoring structure

---
+++ static/node/js/index.js
@@ -0,0 +1,16 @@
+function GetDate() {
+document.getElementById('demo').innerHTML = Date()
+var $myCanvas = $('#MainCanvas');
+
+// rectangle shape
+$myCanvas.drawArc({
+  fillStyle: 'steelblue',
+  strokeStyle: 'steelblue',
+  strokeStyle: 'blue',
+  strokeWidth: 4,
+  x: 100, y: 100,
+  radius: 20,
+  // start and end angles in degrees
+//  start: 0, end: 360
+});
+}
