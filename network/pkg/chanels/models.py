88bcbb2f1b69ccf63bcc9ee68417af2faf60fba1
kryvonis <Kryvonis.artem@gmail.com>
2016-10-27 17:03:25 +0300

    refactoring structure

--- network/pkg/chanels/models.py
+++ network/pkg/chanels/models.py
@@ -1,18 +1,18 @@
-from django.db import models
+# from django.db import models
 import json
 from network.pkg.chanels.serializers import ChanelSerializer
 
 
 # Create your models here.
 class Chanel:
-    def __init__(self, id=0, weight=0, fromX=0, toX=1, fromY=0, toY=1, type='duplex'):
+    def __init__(self, id=0, weight=0, from_x=0, to_x=1, from_y=0, to_y=1, chanel_type='duplex'):
         self.id = id
         self.weight = weight
-        self.fromX = fromX
-        self.toX = toX
-        self.fromY = fromY
-        self.toY = toY
-        self.type = type
+        self.fromX = from_x
+        self.toX = to_x
+        self.fromY = from_y
+        self.toY = to_y
+        self.type = chanel_type
 
     def __str__(self, *args, **kwargs):
         return ','.join((str(value) for value in self.__dict__.values()))
@@ -22,22 +22,3 @@ class Chanel:
         attr = ('%s=%s' % item for item in self.__dict__.items())
 
         return '%s(%s)' % (cls, ', '.join(attr))
-
-    def to_json(self):
-        return ChanelSerializer().encode(self)
-
-
-if __name__ == '__main__':
-    c = Chanel()
-
-    print(c.__repr__())
-    lol = c.to_json()
-    print(type(lol))
-    print(lol)
-    print(ChanelSerializer().encode(c))
-    # t = Chanel().from_json(json.loads(lol))
-    # print(t.__repr__())
-
-    # print(lol['id'])
-    # t = Chanel().deserialize(lol)
-    # print(t.__repr__())
--- network/tests/chanels/test_chanel.py
+++ network/tests/chanels/test_chanel.py
@@ -1,7 +1,20 @@
 from django.test import TestCase
+from network.pkg.chanels.models import Chanel
 
 
 class ChanelTestCase(TestCase):
-    def test_lol(self):
-        print('LOL')
-        self.assertTrue(True)
+    def test_creation(self):
+        obj = Chanel(0, 0, 0, 0, 0, 0, 'duplex')
+        self.assertIsNotNone(obj)
+
+    def test_correct_parameters(self):
+        obj = Chanel(0, 0, 0, 0, 0, 0, 'duplex')
+        self.assertDictEqual(obj.__dict__,
+                             {'id': 0,
+                              'weight': 0,
+                              'fromX': 0,
+                              'toX': 0,
+                              'fromY': 0,
+                              'toY': 0,
+                              'type': 'duplex'
+                              })
